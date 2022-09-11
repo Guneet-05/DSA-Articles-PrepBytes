161d646a707c9d37b1528684754bf92dd2296a3a
Guneet Malhotra <77784585+Guneet-05@users.noreply.github.com>
2022-09-11 20:30:06 +0530

    Create 001 C++ Using Stack.cpp

---
+++ 010 Merge Overlapping Intervals/001 C++ Using Stack.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+struct Interval {
+	int start, end;
+};
+
+
+bool compareInterval(Interval i1, Interval i2)
+{
+	return (i1.start < i2.start);
+}
+
+void mergeIntervals(Interval arr[], int n)
+{
+    
+	if (n <= 0)
+		return;
+
+	
+	stack<Interval> s;
+	
+	sort(arr, arr + n, compareInterval);
+
+	s.push(arr[0]);
+
+	for (int i = 1; i < n; i++) {
+	    
+		Interval top = s.top();
+
+		if (top.end < arr[i].start)
+			s.push(arr[i]);
+
+		else if (top.end < arr[i].end) {
+			top.end = arr[i].end;
+			s.pop();
+			s.push(top);
+		}
+	}
+	cout << "\n The Merged Intervals are: ";
+	while (!s.empty()) {
+		Interval t = s.top();
+		cout << "[" << t.start << "," << t.end << "] ";
+		s.pop();
+	}
+	return;
+}
+
+int main()
+{
+	Interval arr[]
+		= { { 1, 8 }, { 5, 12 }, { 14, 19 }, { 22, 28 }, {25, 27} , {27, 30} };
+	int n = sizeof(arr) / sizeof(arr[0]);
+	mergeIntervals(arr, n);
+	return 0;
+}
