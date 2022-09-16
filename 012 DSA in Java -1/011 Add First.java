e66be06559bf39283406e78de65fc8243c2c87ca
Guneet Malhotra <77784585+Guneet-05@users.noreply.github.com>
2022-09-16 15:37:42 +0530

    Create 011 Add First.java

---
+++ 012 DSA in Java -1/011 Add First.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        LinkedList<Integer> list = new LinkedList<>();
+        list.addFirst(4);
+        list.addFirst(3);
+        list.addFirst(2);
+        list.addFirst(1);
+        
+        System.out.println(list);
+    }
+}
