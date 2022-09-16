421a4d7d0c3104a4932e62ddebf5b2b1fdfcb823
Guneet Malhotra <77784585+Guneet-05@users.noreply.github.com>
2022-09-16 15:30:40 +0530

    Create 010 Add Last in LL.java

---
+++ 012 DSA in Java -1/010 Add Last in LL.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        LinkedList<Integer> list = new LinkedList<>();
+        list.addLast(1);
+        list.addLast(2);
+        list.addLast(3);
+        list.addLast(4);
+        
+        System.out.println(list);
+    }
+}
