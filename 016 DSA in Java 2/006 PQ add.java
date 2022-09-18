0c8d4e6457b06f61d02f60d3e6f86abd1d568239
Guneet Malhotra <77784585+Guneet-05@users.noreply.github.com>
2022-09-18 10:23:29 +0530

    Create 006 PQ add.java

---
+++ 016 DSA in Java 2/006 PQ add.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        
+        PriorityQueue<Integer> pq= new PriorityQueue<>();
+        
+        pq.add(10);
+        pq.add(5);
+        pq.add(50);
+        pq.add(20);
+        pq.add(30);
+
+        System.out.println(pq);
+    }
+}
