648fda806d6d759b5b38927e8c4fa5261f65b3cf
Guneet Malhotra <77784585+Guneet-05@users.noreply.github.com>
2022-09-18 10:21:43 +0530

    Create 005 Priority Queue Creation.java

---
+++ 016 DSA in Java 2/005 Priority Queue Creation.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        //reference and object both of PriorityQueue
+        PriorityQueue<Integer> pq= new PriorityQueue<>();
+        
+        //reference of Queue and object of PriorityQueue
+        Queue<Integer> q = new PriorityQueue<>();
+
+    }
+}
