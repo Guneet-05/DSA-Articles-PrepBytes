d5ab9a76fdd7c36dbab8ffc8635cf2e2c56921be
Guneet Malhotra <77784585+Guneet-05@users.noreply.github.com>
2022-09-18 10:19:49 +0530

    Create 004 Queue size.java

---
+++ 016 DSA in Java 2/004 Queue size.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Queue<Integer> q = new ArrayDeque<>();
+        q.add(10);
+        q.add(20);
+        q.add(30);
+        q.add(40);
+        q.add(50);
+        
+        System.out.println(q);
+        System.out.println(q.size());
+        
+        q.remove();
+        System.out.println(q);
+        System.out.println(q.size());
+        
+        q.remove();
+        System.out.println(q);
+        System.out.println(q.size());
+
+    }
+}
