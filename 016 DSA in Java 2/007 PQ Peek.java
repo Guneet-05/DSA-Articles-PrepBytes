c2895c7d9c8cb1c116bf27089a0a87e6c94b709d
Guneet Malhotra <77784585+Guneet-05@users.noreply.github.com>
2022-09-18 10:24:13 +0530

    Create 007 PQ Peek.java

---
+++ 016 DSA in Java 2/007 PQ Peek.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        
+        PriorityQueue<Integer> pq= new PriorityQueue<>();
+        
+        pq.add(10);
+        pq.add(5);
+        pq.add(50);
+        pq.add(20);
+        pq.add(30);
+
+        System.out.println(pq);
+        System.out.println(pq.peek());
+    }
+}
