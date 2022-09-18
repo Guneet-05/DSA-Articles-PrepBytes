0aba5296824e12356a2a52ef7ba75a343cb2de70
Guneet Malhotra <77784585+Guneet-05@users.noreply.github.com>
2022-09-18 10:15:08 +0530

    Create 001 Queue Creation.java

---
+++ 016 DSA in Java 2/001 Queue Creation.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        //Queue is an Interface
+        //ArrayDeque is a Class
+        //The reference q is of interface Queue
+        //Object is of type ArrayDeque
+        Queue<Integer> q = new ArrayDeque<>();
+    }
+}
