98434f659e8dd4500366588508585a17c75391c7
Guneet Malhotra <77784585+Guneet-05@users.noreply.github.com>
2022-09-18 10:25:51 +0530

    Create 009 Max Heap.java

---
+++ 016 DSA in Java 2/009 Max Heap.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        
+        PriorityQueue<Integer> pq= new PriorityQueue<>(Collections.reverseOrder());
+        
+        pq.add(10);
+        pq.add(5);
+        pq.add(50);
+        pq.add(20);
+        pq.add(30);
+
+        System.out.println(pq);
+        System.out.println(pq.peek());
+        
+        pq.remove();
+        
+        System.out.println(pq);
+        System.out.println(pq.peek());
+        
+    }
+}
