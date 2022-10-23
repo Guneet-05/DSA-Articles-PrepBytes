f4f8110a052f54f5cb765cce29fad8cbf0a70a96
Guneet Malhotra <77784585+Guneet-05@users.noreply.github.com>
2022-10-23 09:30:41 +0530

    Create 004 Static Method.java

---
+++ 26 Interface in Java/004 Static Method.java
@@ -0,0 +1,55 @@
+import java.util.*;
+
+interface i1 {
+    void meth1();
+    void meth2();
+    
+    default void meth3() {
+        System.out.println("This is the default method for this interface");
+    }
+    
+    static void meth4() {
+        System.out.println("Static method can be called without using object");
+    }
+}
+
+class A implements i1{
+    public void meth1() {
+        System.out.println("Class A implementation of meth1");
+    }
+    
+    public void meth2() {
+        System.out.println("Class A implementation of meth2");
+    }
+    
+    public void meth3() {
+        System.out.println("Class A implementation of meth3");
+    }
+}
+
+class B implements i1 {
+    public void meth1() {
+        System.out.println("Class B implementation of meth1");
+    }
+    
+    public void meth2() {
+        System.out.println("Class B implementation of meth2");
+    }
+}
+
+public class Main {
+    
+    public static void main(String[] args) {
+        A objA = new A();
+        objA.meth1();
+        objA.meth2();
+        objA.meth3();
+        
+        B objB = new B();
+        objB.meth1();
+        objB.meth2();
+        objB.meth3();
+        
+        i1.meth4();
+    }
+}
