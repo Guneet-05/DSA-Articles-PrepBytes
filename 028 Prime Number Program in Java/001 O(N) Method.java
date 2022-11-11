e1690a97479a6760a2f04398019811bfa8dd4478
Guneet Malhotra <77784585+Guneet-05@users.noreply.github.com>
2022-11-11 09:49:53 +0530

    Update 001 O(N) Method.java

--- 028 Prime Number Program in Java/001 O(N) Method.java
+++ 028 Prime Number Program in Java/001 O(N) Method.java
@@ -6,6 +6,11 @@ public class Main {
         Scanner scn = new Scanner(System.in);
         int n = scn.nextInt();
         
+        if(n == 1) {
+            System.out.println("NO");
+            return;
+        }
+        
         for(int i=2;i<n;i++) {
             if(n % i == 0) {
                 System.out.println("NO");
