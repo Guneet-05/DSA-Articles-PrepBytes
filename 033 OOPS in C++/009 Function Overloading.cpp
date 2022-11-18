c63032dd36552731e5ab8c57038cbaf5d5782cd7
Guneet Malhotra <77784585+Guneet-05@users.noreply.github.com>
2022-11-18 20:31:23 +0530

    Create 009 Function Overloading.cpp

---
+++ 033 OOPS in C++/009 Function Overloading.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+int add(int a,int b) {
+    return a + b;
+}
+
+int add(int a,int b, int c) {
+    return a + b + c;
+}
+
+
+int main() {
+    cout<<add(2,3)<<"\n";
+    cout<<add(2,3,4)<<"\n";
+}
