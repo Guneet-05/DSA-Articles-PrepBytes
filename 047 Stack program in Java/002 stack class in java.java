cf353bf7419da5a49c3a8bb9db11dc9621f75779
Guneet Malhotra <77784585+Guneet-05@users.noreply.github.com>
2022-12-24 10:51:31 +0530

    Create 002 stack class in java.java

---
+++ 047 Stack program in Java/002 stack class in java.java
@@ -0,0 +1,29 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Stack<Integer> stk = new Stack<>();
+        System.out.println("Pushing some elements in the stack");
+        //pushing an element into the stack -> push()
+        stk.push(10);
+        stk.push(20);
+        stk.push(30);
+        stk.push(40);
+        stk.push(50);
+        //display the stack
+        System.out.println("Stack: " + stk);
+        
+        //top of the stack -> peek()
+        System.out.println("The element at the top of the stack is: " + stk.peek());
+        //size of the stack -> number of elements
+        System.out.println("Number of elements in the stack: " + stk.size());
+        //empty() -> return true if the stack is empty, false otherwise
+        System.out.println("Is the stack empty: " + stk.empty());
+        
+        //Iterating the stack using iterator
+        Iterator it = stk.iterator();
+        while(it.hasNext()) {
+            Object value = it.next();
+            System.out.println(value);
+        }
+    }
+}
