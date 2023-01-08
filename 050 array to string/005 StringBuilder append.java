dd7b269d9fe09444c5f6d159798de31a715c7a1a
Guneet Malhotra <77784585+Guneet-05@users.noreply.github.com>
2023-01-08 11:23:48 +0530

    Create 005 StringBuilder append.java

---
+++ 050 array to string/005 StringBuilder append.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        char[] arr = {'a','e','i','o','u'};
+        StringBuilder sb = new StringBuilder("");
+        sb.append(arr);
+        System.out.println(sb.toString());
+    }
+}
