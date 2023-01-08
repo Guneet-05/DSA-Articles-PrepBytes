7b6a21d635a0100ae72dd985f1dd6bb8b86ea9fc
Guneet Malhotra <77784585+Guneet-05@users.noreply.github.com>
2023-01-08 12:00:06 +0530

    Create 008 c++ string conversion.cpp

---
+++ 050 array to string/008 c++ string conversion.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+string convertToString(char* a)
+{
+	string s(a);
+	return s;
+}
+
+int main()
+{
+	char a[] = { 'a', 'e', 'i', 'o', 'u' };
+
+	string s_a = convertToString(a);
+
+	cout << s_a << endl;
+
+	return 0;
+}
