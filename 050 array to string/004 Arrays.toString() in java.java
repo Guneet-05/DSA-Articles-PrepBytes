883863921c1cbc07a013cbf04a4889ab6d29c318
Guneet Malhotra <77784585+Guneet-05@users.noreply.github.com>
2023-01-08 11:20:21 +0530

    Create 004 Arrays.toString() in java.java

---
+++ 050 array to string/004 Arrays.toString() in java.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        char[] arr1 = {'a','e','i','o','u'};
+        System.out.println(Arrays.toString(arr1));
+        
+        int[] arr2 = {1,2,3,4,5};
+        System.out.println(Arrays.toString(arr2));
+        
+        boolean[] arr3 = {true,true,false,true};
+        System.out.println(Arrays.toString(arr3));
+        
+        String[] arr4 = {"Follow","PrepBytes","for","more","such","content"};
+        System.out.println(Arrays.toString(arr4));
+    }
+}
