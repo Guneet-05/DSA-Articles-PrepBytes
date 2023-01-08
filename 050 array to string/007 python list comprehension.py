852ce0e0963a09729ecfbaaec0fa3bf9be6ce7b7
Guneet Malhotra <77784585+Guneet-05@users.noreply.github.com>
2023-01-08 11:43:14 +0530

    Create 007 python list comprehension.py

---
+++ 050 array to string/007 python list comprehension.py
@@ -0,0 +1,9 @@
+def solve(arr):
+    return (' '.join([str(ele) for ele in arr]))
+
+def main():
+    arr = ['I','have','solved',350,'DSA','questions','out','of',500]
+    print(solve(arr))
+    
+if __name__ == '__main__':
+    main()
